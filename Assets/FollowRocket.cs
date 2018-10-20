a28005e29c5e63f2ca5fb81c48f75843bebfee9c
remvst <remvst@gmail.com>
2018-10-20 18:13:49 -0400

    Follow rocket

---
+++ Assets/FollowRocket.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRocket : MonoBehaviour {
+
+    public GameObject rocket;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = rocket.transform.position + new Vector3(5, 5, 5);
+	}
+}
